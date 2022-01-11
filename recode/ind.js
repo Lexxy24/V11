8ad38fdfb5e1e98602e1aae8e5cf62135b40aa5b
Lexxy official <93019459+Lexxy24@users.noreply.github.com>
2022-01-11 10:31:10 +0700

    Add files via upload

--- recode/ind.js
+++ recode/ind.js
@@ -10,3 +10,142 @@ PULSA: (0822-7991-5237)
 *TERIMA KASIH KAK*
 *LOVE YOU😋*`
 }
+exports.menu = (prefix, simbol) => { 
+return`=====[ 𝗟𝗶𝘀𝘁 𝗠𝗲𝗻𝘂 ]=====
+
+❏ 𝙊𝙩𝙝𝙚𝙧𝙨
+${simbol} ${prefix}infobot
+${simbol} ${prefix}cekprem
+${simbol} ${prefix}scbot
+${simbol} ${prefix}owner
+${simbol} ${prefix}donasi
+${simbol} ${prefix}request <fitur>
+
+❏ 𝙊𝙬𝙣𝙚𝙧
+${simbol} ${prefix}self
+${simbol} ${prefix}public
+${simbol} ${prefix}readall
+${simbol} ${prefix}bc <text>
+${simbol} ${prefix}setfake <reply image>
+${simbol} ${prefix}setreply <nama>
+${simbol} ${prefix}setthumb <reply image>
+${simbol} ${prefix}ban 628xxx
+${simbol} ${prefix}unban 628xxx
+${simbol} ${prefix}addprem 628xxx
+${simbol} ${prefix}delprem 628xxx
+
+❏ 𝙂𝙧𝙤𝙪𝙥
+${simbol} ${prefix}rate
+${simbol} ${prefix}apakah
+${simbol} ${prefix}bisakah
+${simbol} ${prefix}kapankah
+${simbol} ${prefix}antilink
+${simbol} ${prefix}leave
+${simbol} ${prefix}closegc
+${simbol} ${prefix}opengc
+${simbol} ${prefix}linkgrup
+${simbol} ${prefix}listadmin
+${simbol} ${prefix}setpp
+${simbol} ${prefix}kick @tag
+${simbol} ${prefix}add +628
+${simbol} ${prefix}tagall
+${simbol} ${prefix}hidetag
+${simbol} ${prefix}demote
+${simbol} ${prefix}promote
+${simbol} ${prefix}delete
+
+❏ 𝘾𝙤𝙣𝙫𝙚𝙧𝙩
+${simbol} ${prefix}ttp
+${simbol} ${prefix}attp
+${simbol} ${prefix}dare
+${simbol} ${prefix}truth
+${simbol} ${prefix}sticker <reply>
+${simbol} ${prefix}toimg <reply>
+${simbol} ${prefix}cerpen
+${simbol} ${prefix}tongue
+${simbol} ${prefix}pantun
+${simbol} ${prefix}nuliskiri
+${simbol} ${prefix}nuliskanan
+${simbol} ${prefix}foliokiri
+${simbol} ${prefix}foliokanan
+
+❏ 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙
+${simbol} ${prefix}igdl <link>
+${simbol} ${prefix}ttdl <link>
+${simbol} ${prefix}tiktok <link>
+${simbol} ${prefix}ytmp3 <link>
+${simbol} ${prefix}ytmp4 <link>
+${simbol} ${prefix}ytplay <judul>
+${simbol} ${prefix}playmp4 <judul>
+${simbol} ${prefix}playmp3 <judul>
+${simbol} ${prefix}mediafire <link>
+${simbol} ${prefix}instagram <link>
+${simbol} ${prefix}tiktoknowm <link>
+
+❏ 𝙎𝙚𝙖𝙧𝙘𝙝
+${simbol} ${prefix}waifu
+${simbol} ${prefix}husbu
+${simbol} ${prefix}herolist
+${simbol} ${prefix}herodetail
+${simbol} ${prefix}ytsearch
+${simbol} ${prefix}pinterest
+${simbol} ${prefix}playstore
+${simbol} ${prefix}nekonime
+${simbol} ${prefix}namaninja
+${simbol} ${prefix}imgsearch
+${simbol} ${prefix}searchgrup
+
+❏ 𝘾𝙖𝙣𝙫𝙖𝙨
+${simbol} ${prefix}rem
+${simbol} ${prefix}gura
+${simbol} ${prefix}serti1
+${simbol} ${prefix}serti2
+${simbol} ${prefix}serti3
+${simbol} ${prefix}kaneki
+${simbol} ${prefix}sadboy
+${simbol} ${prefix}girlneko
+${simbol} ${prefix}lolimaker
+
+❏ 𝙍𝙖𝙣𝙙𝙤𝙢
+${simbol} ${prefix}fox
+${simbol} ${prefix}cat
+${simbol} ${prefix}dog
+${simbol} ${prefix}birb
+${simbol} ${prefix}koala
+${simbol} ${prefix}panda
+${simbol} ${prefix}meme
+${simbol} ${prefix}nickepep
+${simbol} ${prefix}katailham
+${simbol} ${prefix}red_panda
+${simbol} ${prefix}estetikpic
+${simbol} ${prefix}darkjokes
+${simbol} ${prefix}memeindo
+${simbol} ${prefix}katasindiran
+
+❏ 𝙉𝙨𝙛𝙬
+${simbol} ${prefix}trap
+${simbol} ${prefix}neko
+${simbol} ${prefix}awoo
+${simbol} ${prefix}waifu
+${simbol} ${prefix}blowjob
+${simbol} ${prefix}megumin
+${simbol} ${prefix}shinobu
+
+❏ 𝙈𝙖𝙠𝙚𝙧
+${simbol} ${prefix}bneon
+${simbol} ${prefix}matrix
+${simbol} ${prefix}breakwall
+${simbol} ${prefix}dropwater
+${simbol} ${prefix}lithgtext
+${simbol} ${prefix}crismes
+${simbol} ${prefix}tfire
+${simbol} ${prefix}sandw
+${simbol} ${prefix}text3dbox
+${simbol} ${prefix}leavest
+${simbol} ${prefix}tlight
+${simbol} ${prefix}nulis
+${simbol} ${prefix}blackpink
+${simbol} ${prefix}neon_light
+${simbol} ${prefix}gaming
+${simbol} ${prefix}watercolor`
+}
